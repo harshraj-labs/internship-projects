8c32c15dea9d2df1c72deb34d81bf6768fa174c1
Harsh Raj <harshraj.contact@gmail.com>
2026-08-09 23:51:57 +0530

    Docker Compose Work

--- assignment 1/server.js
+++ assignment 1/server.js
@@ -1,6 +1,19 @@
+require('dotenv').config();
 const express = require('express');
+const {Client} = require('pg');
 const Database = require('better-sqlite3');
 const app = express();
+const client = new Client({
+    connectionString: process.env.DATABASE_URL
+});
+
+client.connect()
+    .then(() => {
+        console.log("Connected to PostgreSQL");
+    })
+    .catch((err) => {
+        console.error("PostgreSQL connection failed:", err);
+    });
 const db = new Database('tasks.db');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./openapi.json');
